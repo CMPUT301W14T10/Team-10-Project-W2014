6d698d6e189dae897a3a13a07a9c0626d7560690
chickendiver <bradley.poulette@gmail.com>
2014-02-20 15:37:08 -0700

    Added LocationModel.java with basic variables.

---
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/LocationModel.java
@@ -0,0 +1,47 @@
+package ca.ualberta.team10projectw2014;
+
+public class LocationModel 
+{
+	private String name;
+	private String latitude;
+	private String longitude;
+
+	public LocationModel(String name, String latitude, String longitude)
+	{
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.name = name;
+		
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public String getLatitude() 
+	{
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) 
+	{
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() 
+	{
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) 
+	{
+		this.longitude = longitude;
+	}
+	
+}
