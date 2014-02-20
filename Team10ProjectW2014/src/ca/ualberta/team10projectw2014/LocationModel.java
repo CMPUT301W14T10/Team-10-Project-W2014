483ee6edcbf458490f2d4a51a280fe96c7dd917f
chickendiver <bradley.poulette@gmail.com>
2014-02-20 15:41:39 -0700

    Changed opening braces to conform with coding convention.

--- Team10ProjectW2014/src/ca/ualberta/team10projectw2014/LocationModel.java
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/LocationModel.java
@@ -1,46 +1,38 @@
 package ca.ualberta.team10projectw2014;
 
-public class LocationModel 
-{
+public class LocationModel {
 	private String name;
 	private String latitude;
 	private String longitude;
 
-	public LocationModel(String name, String latitude, String longitude)
-	{
+	public LocationModel(String name, String latitude, String longitude) {
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.name = name;
 		
 	}
 
-	public String getName() 
-	{
+	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) 
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getLatitude() 
-	{
+	public String getLatitude() {
 		return latitude;
 	}
 
-	public void setLatitude(String latitude) 
-	{
+	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
 
-	public String getLongitude() 
-	{
+	public String getLongitude() {
 		return longitude;
 	}
 
-	public void setLongitude(String longitude) 
-	{
+	public void setLongitude(String longitude) {
 		this.longitude = longitude;
 	}
 	
