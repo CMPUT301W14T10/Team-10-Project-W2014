34dd63f2d00a4a8d72d1862c22727ff351af20b7
chickendiver <bradley.poulette@gmail.com>
2014-02-23 15:13:01 -0700

    Worked on tests.

--- Team10ProjectW2014/src/ca/ualberta/team10projectw2014/CreateCommentActivityTests.java
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/CreateCommentActivityTests.java
@@ -24,15 +24,25 @@ public class CreateCommentActivityTests extends
 		activity = getActivity();
 		
 		contentText = ((EditText) activity.findViewById(ca.ualberta.team10projectw2014.R.id.cc_content));
-		titleText = ((EditText) activity.findViewById(ca.ualberta.cs.team10projectw2014.R.id.cc_title));
-		usernameText = ((EditText) activity.findViewById(ca.ualberta.cs.team10projectw2014.R.id.cc_username));
+		titleText = ((EditText) activity.findViewById(ca.ualberta.team10projectw2014.R.id.cc_title));
+		usernameText = ((EditText) activity.findViewById(ca.ualberta.team10projectw2014.R.id.cc_username));
 	}
 	
-	public void testCreateNewHeadCommentWithoutUsername(){
-		CreateCommentActivity activity = getActivity();
+	public void testCreateNewHeadCommentWithoutUsername() throws Throwable{
 		
-		activity.fillContents();
-		
+		runTestOnUiThread(new Runnable(){
+			@Override
+			public void run(){
+				CreateCommentActivity activity = getActivity();
+				
+				activity.fillContents();
+				
+				assertEquals("contentText should be blank", contentText.getText().toString(), "");
+				assertEquals("titleText should be blank", titleText.getText().toString(), "");
+				assertEquals("usernameText should be blank", usernameText.getText().toString(), "");
+				
+			}
+		});
 		
 	}
 
