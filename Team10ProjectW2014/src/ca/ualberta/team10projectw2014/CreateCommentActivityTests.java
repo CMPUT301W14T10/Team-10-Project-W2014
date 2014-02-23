1cbd025a33fea3301662666ce7d5505f28f62689
chickendiver <bradley.poulette@gmail.com>
2014-02-23 15:06:43 -0700

    Started work on the CC tests.

--- Team10ProjectW2014/src/ca/ualberta/team10projectw2014/CreateCommentActivityTests.java
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/CreateCommentActivityTests.java
@@ -1,12 +1,39 @@
 package ca.ualberta.team10projectw2014;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
 
 public class CreateCommentActivityTests extends
 		ActivityInstrumentationTestCase2<CreateCommentActivity> {
+	
+	Instrumentation instrumentation;
+	Activity activity;
+	EditText usernameText;
+	EditText contentText;
+	EditText titleText;
 
 	public CreateCommentActivityTests() {
 		super(CreateCommentActivity.class);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		instrumentation = getInstrumentation();
+		activity = getActivity();
+		
+		contentText = ((EditText) activity.findViewById(ca.ualberta.team10projectw2014.R.id.cc_content));
+		titleText = ((EditText) activity.findViewById(ca.ualberta.cs.team10projectw2014.R.id.cc_title));
+		usernameText = ((EditText) activity.findViewById(ca.ualberta.cs.team10projectw2014.R.id.cc_username));
+	}
+	
+	public void testCreateNewHeadCommentWithoutUsername(){
+		CreateCommentActivity activity = getActivity();
+		
+		activity.fillContents();
+		
+		
 	}
 
 }
