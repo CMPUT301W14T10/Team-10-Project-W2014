243e2ffa0cb54302d44cd5947bb29c816074bcb1
chickendiver <bradley.poulette@gmail.com>
2014-02-23 14:07:55 -0700

    Added Added CreateCommentActivityTests and added the operating class to the manifest.

--- Team10ProjectW2014/src/ca/ualberta/team10projectw2014/CreateCommentActivity.java
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/CreateCommentActivity.java
@@ -1,5 +1,7 @@
 package ca.ualberta.team10projectw2014;
 
-public class CreateCommentActivity {
+import android.app.Activity;
+
+public class CreateCommentActivity extends Activity{
 
 }
---
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/CreateCommentActivityTests.java
@@ -0,0 +1,12 @@
+package ca.ualberta.team10projectw2014;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class CreateCommentActivityTests extends
+		ActivityInstrumentationTestCase2<CreateCommentActivity> {
+
+	public CreateCommentActivityTests() {
+		super(CreateCommentActivity.class);
+	}
+
+}
