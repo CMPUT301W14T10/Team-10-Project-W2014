570e4931eab02d5fcf8c44acadc31800360ccc91
czervos <czervos@ualberta.ca>
2014-02-27 20:34:01 -0700

    Added methods required by BaseAdapter abstract class

--- Team10ProjectW2014/src/ca/ualberta/team10projectw2014/MainListViewAdapter.java
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/MainListViewAdapter.java
@@ -1,5 +1,72 @@
+/**
+ * Copyright 2014 Cole Fudge, Steven Giang, Bradley Poullet, David Yee, and Costa Zervos
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 package ca.ualberta.team10projectw2014;
 
-public class MainListViewAdapter {
+import java.util.ArrayList;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Creates a custom adapter for displaying three textviews and an imageview for 
+ * each item in a listview.
+ */
+public class MainListViewAdapter extends BaseAdapter {
+
+	private LayoutInflater inflater;
+	private ArrayList<HeadModel> HeadCommentList;
+	
+	/**
+	 * Gets the number of counters in the array of counter models.
+	 * @return the number of counters in the list as an int.
+	 */
+	public int getCount() {
+		return HeadCommentList.size();
+	}
+	
+	/**
+	 * Gets the view to be displayed by the listView.
+	 * @return view to be displayed. 
+	 */
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO complete method
+		return convertView;
+	}
+	
+	/**
+	 * Gets the head comment at a specific position.
+	 * @return the head comment.
+	 */
+	public HeadModel getItem(int position) {
+		return HeadCommentList.get(position);
+		}
+	
+	/**
+	 * Gets the position (method needed by class type).
+	 * @return position as a long.
+	 */
+	public long getItemId(int position) {
+		return position;
+		}
+	
+	
+	
 }
