343281399b3462bdfe6d26f55d56a517f14e531b
sgiang92 <giang2@ualberta.ca>
2014-03-02 20:53:59 -0700

    Added test file for SubCommentViewActivity

---
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/SubCommentViewActivityTests.java
@@ -0,0 +1,14 @@
+package ca.ualberta.team10projectw2014;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class SubCommentViewActivityTests extends
+		ActivityInstrumentationTestCase2<SubCommentViewActivity> {
+
+	public SubCommentViewActivityTests() {
+		super(SubCommentViewActivity.class);
+	}
+	
+	
+
+}
