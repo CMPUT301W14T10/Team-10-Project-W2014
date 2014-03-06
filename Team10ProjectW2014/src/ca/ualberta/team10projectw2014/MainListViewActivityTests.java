338677bc6977272eaeeeba8a26c5da9b8268b732
czervos <czervos@ualberta.ca>
2014-03-06 16:20:52 -0700

    Created MainListViewActivityTests

---
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/MainListViewActivityTests.java
@@ -0,0 +1,20 @@
+package ca.ualberta.team10projectw2014;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListView;
+
+public class MainListViewActivityTests extends
+		ActivityInstrumentationTestCase2<MainListViewActivity> {
+	
+	public MainListViewActivityTests() {
+		super(MainListViewActivity.class);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
