3fb47124c717c3b60ff48760cb05cd165e7b9614
czervos <czervos@ualberta.ca>
2014-03-06 16:48:30 -0700

    Started display comment in list test for MainListViewActivityTests

--- Team10ProjectW2014/src/ca/ualberta/team10projectw2014/MainListViewActivityTests.java
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/MainListViewActivityTests.java
@@ -11,6 +11,9 @@ import android.widget.ListView;
 public class MainListViewActivityTests extends
 		ActivityInstrumentationTestCase2<MainListViewActivity> {
 	
+	Activity activity;
+	ListView headListView;
+	
 	public MainListViewActivityTests() {
 		super(MainListViewActivity.class);
 	}
@@ -19,6 +22,37 @@ public class MainListViewActivityTests extends
 		super.setUp();
 	}
 	
+	/**
+	 * Test to confirm head comment list displays a comment correctly
+	 * 
+	 * @throws Throwable
+	 */
+	public void displayCommentInList() throws Throwable {
+		// Create a test head comment
+		HeadModel headComment = new HeadModel();
+		headComment.setTitle("Test Title");
+		headComment.setAuthor("TestUsername");
+		headComment.setContent("Test Head Comment Content");
+		headComment.setTimestamp(Calendar.getInstance());
+		
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setClassName("ca.ualberta.team10projectw2014", 
+        		"ca.ualberta.team10projectw2014.MainListViewActivity");
+        intent.putExtra("HeadComment", headComment);
+        setActivityIntent(intent);
+        
+        activity = getActivity();
+        
+        headListView = (ListView) activity.findViewById(ca.ualberta.team10projectw2014.R.id.HeadCommentList);
+        // TODO assert equal title matches textview containing title
+        // TODO assert equal title matches textview containing username
+        // TODO assert equal title matches textview containing location
+        // TODO assert equal title matches textview containing date
+        
+        activity.finish();
+        
+        setActivity(null);
+	}
 	
 	
 	/**
