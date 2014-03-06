485bb3bd1cfd2e6448be04c081301ee3ae505c3d
chickendiver <bradley.poulette@gmail.com>
2014-03-06 15:43:37 -0700

    Started work on location provider

---
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/LocationListenerController.java
@@ -0,0 +1,37 @@
+package ca.ualberta.team10projectw2014;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+public class LocationListenerController implements LocationListener {
+
+	public LocationListenerController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
