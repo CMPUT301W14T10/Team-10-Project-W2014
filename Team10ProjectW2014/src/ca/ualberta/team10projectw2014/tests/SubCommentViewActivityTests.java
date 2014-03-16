575b7f445a16bf7aa5e90a7dcd4062f4e6b17199
sgiang92 <giang2@ualberta.ca>
2014-03-16 15:11:38 -0600

    deleted test file in main project package

--- Team10ProjectW2014/src/ca/ualberta/team10projectw2014/tests/SubCommentViewActivityTests.java
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/tests/SubCommentViewActivityTests.java
@@ -1,5 +1,6 @@
 package ca.ualberta.team10projectw2014.tests;
 
+import ca.ualberta.team10projectw2014.ApplicationStateModel;
 import ca.ualberta.team10projectw2014.SubCommentViewActivity;
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
@@ -8,29 +9,20 @@ public class SubCommentViewActivityTests extends
         ActivityInstrumentationTestCase2<SubCommentViewActivity> {
     
     Activity activity;
+    ApplicationStateModel appState;
 
-    public SubCommentViewActivityTests(
-            Class<SubCommentViewActivity> activityClass) {
-        super(activityClass);
-    }
-    
-    /*
-     * Crashes if this is added but it is required!
-     */
-     
-    /*
     public SubCommentViewActivityTests() {
         super(SubCommentViewActivity.class);
     }
-    */
+ 
     
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        activity = getActivity();
     }
     
-    public void testNewHeadComment() throws Exception {
+    public void testShowNewHeadComment() throws Exception {
+    	
         assertEquals("true",true,true);
     }
 }
