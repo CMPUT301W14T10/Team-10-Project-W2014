849759c35d7368e6527f95a75bdb2985a3a3cf15
czervos <czervos@ualberta.ca>
2014-03-16 17:26:24 -0600

    Cleaned up SubCommentModel

--- Team10ProjectW2014/src/ca/ualberta/team10projectw2014/models/SubCommentModel.java
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/models/SubCommentModel.java
@@ -1,52 +1,34 @@
-/**
- * Copyright 2014 Cole Fudge, Steven Giang, Bradley Poullet, David Yee, and Costa Zervos
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
-
 package ca.ualberta.team10projectw2014.models;
 
 /**
  * This class acts as the model for the sub comments.
- * @author Costa Zervos
- * @version     1                (current version number of program)
+ * @author      Costa Zervos <czervos@ualberta.ca>
+ * @version     1            (current version number of program)
  */
 public class SubCommentModel extends CommentModel{
 	private String parentTitle;
 
+	/**
+	 * Retrieves the parent title as a string
+	 * @return the parent title as a string
+	 */
 	public String getParentTitle() {
 		return parentTitle;
 	}
 
+	/**
+	 * Sets the parent title
+	 * @param parentTitle string to set the title to
+	 */
 	public void setParentTitle(String parentTitle) {
 		this.parentTitle = parentTitle;
 	}
 
 	/**
-	 * Constructor method that sets the comment this sub comment is responding to.
+	 * Constructor method
 	 * @param text the String to name the counter.
 	 */
-	
 	public SubCommentModel(CommentModel parentComment) {
 		//this.respondedTo = parentComment;
-	}
-	
-	//public CommentModel getRespondedTo() {
-		//return respondedTo;
-	//}
-
-	
-	public void setRespondedTo(CommentModel respondedTo) {
-		//this.respondedTo = respondedTo;
 	}
 }
