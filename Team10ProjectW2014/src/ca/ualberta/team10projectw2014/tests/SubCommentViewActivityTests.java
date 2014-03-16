64e1ca0092b5c88ba7e055347add8337552f7531
sgiang92 <giang2@ualberta.ca>
2014-03-16 15:55:18 -0600

    working on SCVAtest

--- Team10ProjectW2014/src/ca/ualberta/team10projectw2014/tests/SubCommentViewActivityTests.java
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/tests/SubCommentViewActivityTests.java
@@ -1,28 +1,52 @@
 package ca.ualberta.team10projectw2014.tests;
 
-import ca.ualberta.team10projectw2014.ApplicationStateModel;
-import ca.ualberta.team10projectw2014.SubCommentViewActivity;
+import java.util.ArrayList;
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ArrayAdapter;
+import ca.ualberta.team10projectw2014.ApplicationStateModel;
+import ca.ualberta.team10projectw2014.CommentModel;
+import ca.ualberta.team10projectw2014.LocationModel;
+import ca.ualberta.team10projectw2014.SubCommentViewActivity;
 
 public class SubCommentViewActivityTests extends
-        ActivityInstrumentationTestCase2<SubCommentViewActivity> {
-    
-    Activity activity;
-    ApplicationStateModel appState;
+		ActivityInstrumentationTestCase2<SubCommentViewActivity> {
 
-    public SubCommentViewActivityTests() {
-        super(SubCommentViewActivity.class);
-    }
- 
-    
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
-    
-    public void testShowNewHeadComment() throws Exception {
-    	
-        assertEquals("true",true,true);
-    }
+	Activity activity;
+	ApplicationStateModel appState;
+
+	public SubCommentViewActivityTests() {
+		super(SubCommentViewActivity.class);
+		appState = ApplicationStateModel.getInstance();
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testShowHeadComment() throws Exception {
+
+
+		CommentModel headComment = new CommentModel();
+		headComment.setAuthor("test author");
+		headComment.setTitle("test title");
+		headComment.setTimestamp(Calendar.getInstance());
+		headComment.setContent("Body");
+		headComment.setLocation(new LocationModel("Test Location name",10.4,10.4));
+	
+		appState.setSubCommentViewHead(headComment);
+		
+		Activity subCommentViewActivity = getActivity();
+		
+		
+
+
+		
+		
+		
+		
+		}
 }
