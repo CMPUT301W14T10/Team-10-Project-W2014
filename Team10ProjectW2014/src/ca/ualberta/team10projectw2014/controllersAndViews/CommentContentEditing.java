0bb527965ddce77a61f7a3663cd14b3a7f13ffaf
chickendiver <bradley.poulette@gmail.com>
2014-03-17 13:27:32 -0600

    Added javaDoc to CommentContentEditing interface

--- Team10ProjectW2014/src/ca/ualberta/team10projectw2014/controllersAndViews/CommentContentEditing.java
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/controllersAndViews/CommentContentEditing.java
@@ -2,6 +2,16 @@ package ca.ualberta.team10projectw2014.controllersAndViews;
 
 import android.view.View;
 
+/**
+ * @author      Bradley Poulette <bpoulett@ualberta.ca>
+ * @version     1                (current version number of program)
+ * 
+ * <p>
+ * This interface gives an outline of the basic functionality of both {@link #CreateCommentActivity} and {@link #EditCommentActivity}
+ * 
+ * All of these methods are described more in detail in the implementing classes' files.
+ * 
+*/
 public interface CommentContentEditing {
 	
 	public void attemptCommentCreation(View v);
