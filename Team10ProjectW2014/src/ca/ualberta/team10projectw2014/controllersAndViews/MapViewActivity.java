2a264244037aa2443021e4790117d0b127063b35
chickendiver <bradley.poulette@gmail.com>
2014-04-01 00:27:11 -0600

    Added MapViewActivity and adjusted Manifest to run it first. Can't get emulator running on my home PC...

---
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/controllersAndViews/MapViewActivity.java
@@ -0,0 +1,55 @@
+package ca.ualberta.team10projectw2014.controllersAndViews;
+
+import ca.ualberta.team10projectw2014.R;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MapViewActivity extends Activity {
+ 
+    // Google Map
+    private GoogleMap googleMap;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_view);
+ 
+        try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+    }
+ 
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+ 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
+ 
+}
