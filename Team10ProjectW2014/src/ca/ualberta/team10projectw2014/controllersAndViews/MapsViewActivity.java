0acfffa22b27ca2b6cfa4b633ff1aaa897d6c498
chickendiver <bradley.poulette@gmail.com>
2014-04-05 20:56:50 -0600

    Finished mapping functionality

--- Team10ProjectW2014/src/ca/ualberta/team10projectw2014/controllersAndViews/MapsViewActivity.java
+++ Team10ProjectW2014/src/ca/ualberta/team10projectw2014/controllersAndViews/MapsViewActivity.java
@@ -9,6 +9,8 @@ import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import ca.ualberta.team10projectw2014.R;
+import ca.ualberta.team10projectw2014.models.ApplicationStateModel;
+import ca.ualberta.team10projectw2014.models.CommentModel;
 
 import com.mapquest.android.maps.AnnotationView;
 import com.mapquest.android.maps.DefaultItemizedOverlay;
@@ -24,7 +26,9 @@ import com.mapquest.android.maps.PolygonOverlay;
 public class MapsViewActivity extends MapActivity{
 
 	protected MapView map;
-    AnnotationView annotation;
+    private AnnotationView annotation;
+    private List<GeoPoint> poiLocs;
+    private ApplicationStateModel appState;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,12 +42,35 @@ public class MapsViewActivity extends MapActivity{
 
       // initialize the annotation to be shown later 
       annotation = new AnnotationView(map);
-
-      addPoiOverlay();
+    }
+    
+    @Override
+    public void onResume(){
+    	super.onResume();
+    	appState = ApplicationStateModel.getInstance();
+    	List<OverlayItem> locationList = generateCoords();
+        addPoiOverlay(locationList);
+    }
+    
+    private List<OverlayItem> generateCoords(){
+    	//Get list of current stuff from AppState
+    	//Take coordinates and descriptions from that list to create new GeoPoints
+    	
+    	List<CommentModel> commentList = appState.getSubCommentViewHead().getSubComments();
+    	List<OverlayItem> locationsList = new ArrayList<OverlayItem>();
+    	CommentModel headComment = appState.getSubCommentViewHead();
+    	map.getController().setCenter(new GeoPoint(headComment.getLocation().getLatitude(),headComment.getLocation().getLongitude()));
+    	locationsList.add(new OverlayItem(new GeoPoint (headComment.getLocation().getLatitude(), headComment.getLocation().getLongitude()), headComment.getLocation().getName().toString(), ""));
+    	for(int i = 0; i < commentList.size();i++){
+    		locationsList.add(new OverlayItem(new GeoPoint (commentList.get(i).getLocation().getLatitude(), commentList.get(i).getLocation().getLongitude()), commentList.get(i).getLocation().getName().toString(), ""));
+    	}
+    	return locationsList;
     }
 	
     // add an itemized overlay to map 
-    private void addPoiOverlay() {
+    private void addPoiOverlay(List<OverlayItem> locationList) {
+    	
+      List<OverlayItem> locList = locationList;
 
       // use a custom POI marker by referencing the bitmap file directly,
       // using the filename as the resource ID 
@@ -51,8 +78,9 @@ public class MapsViewActivity extends MapActivity{
       final DefaultItemizedOverlay poiOverlay = new DefaultItemizedOverlay(icon);
 
       // set GeoPoints and title/snippet to be used in the annotation view 
-      OverlayItem poi1 = new OverlayItem(new GeoPoint (53.52676,-113.52715), "Computing Science Centre", "");
-      poiOverlay.addItem(poi1);
+      for (int i = 0; i < locList.size();i++){
+    	  poiOverlay.addItem(locList.get(i));
+      }
 
       // add a tap listener for the POI overlay 
       poiOverlay.setTapListener(new ItemizedOverlay.OverlayTapListener() {
